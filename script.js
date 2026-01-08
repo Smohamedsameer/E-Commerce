a3ba30dd95c2960695d8ee6c9fc7690eed4448ae
Smohamedsameer <sameerkaleemi10@gmail.com>
2026-01-08 12:39:09 +0530

    new update

---
+++ script.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
+
+if (bar) {
+  bar.onclick = () => {
+    nav.classList.add("active");
+  };
+}
+
+if (close) {
+  close.onclick = () => {
+    nav.classList.remove("active");
+  };
+}
