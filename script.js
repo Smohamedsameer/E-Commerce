a8e7d7bb9282b3689c0d1c1c3e888ce97c381296
Smohamedsameer <sameerkaleemi10@gmail.com>
2026-01-16 22:27:55 +0530

    updated

--- script.js
+++ script.js
@@ -14,28 +14,29 @@ if (close) {
   };
 }
 
-const addCartButtons = document.querySelectorAll(".normal");
+// const addCartButtons = document.querySelectorAll(".normal");
 
-addCartButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    const product = {
-      name: button.dataset.name,
-      price: button.dataset.price,
-      image: button.dataset.image,
-      quantity: 1
-    };
+// addCartButtons.forEach(button => {
+//   button.addEventListener("click", () => {
+//     const product = {
+//       name: button.dataset.name,
+//       price: button.dataset.price,
+//       image: button.dataset.image,
+//       quantity: 1
+//     };
 
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
-    cart.push(product);
-    localStorage.setItem("cart", JSON.stringify(cart));
+//     let cart = JSON.parse(localStorage.getItem("cart")) || [];
+//     cart.push(product);
+//     localStorage.setItem("cart", JSON.stringify(cart));
 
-    alert("Product added to cart");
-  });
-});
+//     alert("Product added to cart");
+//   });
+// });
 
 
 
-console.log("menu script loaded");
-cart.forEach(item => {
-  console.log(item.image); // ✅ valid
-});
+// console.log("menu script loaded");
+// cart.forEach(item => {
+//   console.log(item.image); // ✅ valid
+// });
+
